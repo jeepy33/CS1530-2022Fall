1efe1c6eaff798d216fad30e87a6cf4f60cfd579
tylercomisky <tdcomisky@gmail.com>
2022-10-27 17:11:51 -0400

    Initialized Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+// put this somewhere:
+/*
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+
+*/
 
 void main() {
   runApp(const MyApp());
