a6b8fe5e933aa52ae454b2270a52ebccdaa3a794
Jagr Krtanjek <jagr@Jagrs-MacBook-Air.local>
2022-11-20 15:10:13 -0500

    update feature branch

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,13 @@
 const String homeRoute = '/home';
 const String filterRoute = '/filter';
+const int green = 0x86c66f;
+const int grey = 0x8d948b;
+const List<String> diceImageList = [
+  'assets/dice_idle.png',
+  'assets/dice_1.png',
+  'assets/dice_2.png',
+  'assets/dice_3.png',
+  'assets/dice_4.png',
+  'assets/dice_5.png',
+  'assets/dice_6.png',
+];
