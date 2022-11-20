298115197af3eb74db9b444684b657f421225047
alexaspaventa <alexaspaventa@gmail.com>
2022-11-20 15:14:23 -0500

    Adding filterchips

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,16 +1,2 @@
 const String homeRoute = '/home';
 const String filterRoute = '/filter';
-
-const List<String> list = [
-  "One",
-  "Two",
-  "Three",
-  "Four",
-  "Five",
-  "Six",
-  "Seven",
-  "Eight",
-  "Nine",
-  "Ten"
-];
-List<String> selectedCountList = [];
---
+++ lib/list.dart
@@ -0,0 +1,13 @@
+List<String> countList = [
+  "One",
+  "Two",
+  "Three",
+  "Four",
+  "Five",
+  "Six",
+  "Seven",
+  "Eight",
+  "Nine",
+  "Ten"
+];
+List<String> selectedCountList = [];
