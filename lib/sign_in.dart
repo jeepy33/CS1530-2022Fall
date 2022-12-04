f648dbe61e6caf0e18bfd0728514f50d10e6b53c
alexaspaventa <alexaspaventa@gmail.com>
2022-12-04 13:49:25 -0500

    Styles for login and signup

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -24,12 +24,13 @@ class _SignInPageState extends State<SignInPage> {
 
   Future<FirebaseApp> _initializeFirebase() async {
     FirebaseOptions firebaseConfig = DefaultFirebaseOptions.currentPlatform;
-    FirebaseApp firebaseApp = await Firebase.initializeApp(options: firebaseConfig);
+    FirebaseApp firebaseApp =
+        await Firebase.initializeApp(options: firebaseConfig);
     User? user = FirebaseAuth.instance.currentUser;
 
     if (user != null && mounted) {
       //Navigator.pushNamed(context, homeRoute,
-          //arguments: 'arguments/chose Templates');
+      //arguments: 'arguments/chose Templates');
     }
 
     return firebaseApp;
@@ -43,9 +44,10 @@ class _SignInPageState extends State<SignInPage> {
         _focusPassword.unfocus();
       },
       child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 119, 195, 91),
         appBar: AppBar(
-          title: Text('Firebase Authentication'),
-        ),
+            backgroundColor: Color.fromARGB(255, 119, 174, 98),
+            title: const Text('LocAte')),
         body: FutureBuilder(
           future: _initializeFirebase(),
           builder: (context, snapshot) {
@@ -55,18 +57,23 @@ class _SignInPageState extends State<SignInPage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 24.0),
-                      child: Text(
-                        'Login',
-                        style: Theme.of(context).textTheme.headline1,
-                      ),
+                    Text(
+                      'Login',
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 100, // <-- SEE HERE
                     ),
                     Form(
                       key: _formKey,
                       child: Column(
                         children: <Widget>[
                           TextFormField(
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                            cursorColor: Colors.white,
                             controller: _emailTextController,
                             focusNode: _focusEmail,
                             validator: (value) => Validator.validateEmail(
@@ -74,6 +81,9 @@ class _SignInPageState extends State<SignInPage> {
                             ),
                             decoration: InputDecoration(
                               hintText: "Email",
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
                               errorBorder: UnderlineInputBorder(
                                 borderRadius: BorderRadius.circular(6.0),
                                 borderSide: BorderSide(
@@ -84,6 +94,8 @@ class _SignInPageState extends State<SignInPage> {
                           ),
                           SizedBox(height: 8.0),
                           TextFormField(
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                            cursorColor: Colors.white,
                             controller: _passwordTextController,
                             focusNode: _focusPassword,
                             obscureText: true,
@@ -92,6 +104,9 @@ class _SignInPageState extends State<SignInPage> {
                             ),
                             decoration: InputDecoration(
                               hintText: "Password",
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
                               errorBorder: UnderlineInputBorder(
                                 borderRadius: BorderRadius.circular(6.0),
                                 borderSide: BorderSide(
@@ -102,48 +117,63 @@ class _SignInPageState extends State<SignInPage> {
                           ),
                           SizedBox(height: 24.0),
                           _isProcessing
-                              ? CircularProgressIndicator()
+                              ? CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
                               : Row(
-                            mainAxisAlignment:
-                            MainAxisAlignment.spaceBetween,
-                            children: [
-                              Expanded(
-                                child: ElevatedButton(
-                                  onPressed: () async {
-                                    _focusEmail.unfocus();
-                                    _focusPassword.unfocus();
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Color.fromARGB(
+                                                255, 255, 255, 255),
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 150, vertical: 30),
+                                            textStyle: const TextStyle(
+                                                fontSize: 25,
+                                                fontWeight: FontWeight.bold)),
+                                        onPressed: () async {
+                                          _focusEmail.unfocus();
+                                          _focusPassword.unfocus();
 
-                                    if (_formKey.currentState!
-                                        .validate()) {
-                                      setState(() {
-                                        _isProcessing = true;
-                                      });
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            setState(() {
+                                              _isProcessing = true;
+                                            });
 
-                                      User? user = await FireAuth
-                                          .signInUsingEmailPassword(
-                                        email: _emailTextController.text,
-                                        password:
-                                        _passwordTextController.text, context: context,
-                                      );
+                                            User? user = await FireAuth
+                                                .signInUsingEmailPassword(
+                                              email: _emailTextController.text,
+                                              password:
+                                                  _passwordTextController.text,
+                                              context: context,
+                                            );
 
-                                      setState(() {
-                                        _isProcessing = false;
-                                      });
+                                            setState(() {
+                                              _isProcessing = false;
+                                            });
 
-                                      if (user != null && mounted) {
-                                        Navigator.pushNamed(context, homeRoute,
-                                            arguments: 'arguments/chose Templates');
-                                      }
-                                    }
-                                  },
-                                  child: Text(
-                                    'Sign In',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          )
+                                            if (user != null && mounted) {
+                                              Navigator.pushNamed(
+                                                  context, homeRoute,
+                                                  arguments:
+                                                      'arguments/chose Templates');
+                                            }
+                                          }
+                                        },
+                                        child: Text(
+                                          'Sign In',
+                                          style: TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 119, 195, 91)),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
                         ],
                       ),
                     )
