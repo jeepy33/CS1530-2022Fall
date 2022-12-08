c97021061bdcabfac5927a61b831913b5ab6e04d
halanubani <60750262+halanubani@users.noreply.github.com>
2022-12-08 04:00:08 -0500

    fixing the merge conflict

--- lib/CheckIn.dart
+++ lib/CheckIn.dart
@@ -3,16 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
-<<<<<<< HEAD
 import 'dart:async';
 import 'locations.dart' as locations;
 
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-=======
 import 'firebase_options.dart';
 import 'globals.dart';
->>>>>>> main
 
 class CheckPage extends StatefulWidget {
   // This widget is the home page of your application. It is stateful, meaning
@@ -38,29 +35,26 @@ class _CheckInPageState extends State<CheckPage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
   List<Restaurant> restList = [];
 
-
   // creates a list of all the restaurants in the database
   void test() async {
-    await db.collection("restaurants").get().then((event) {
-      int i = 0;
-      for (var doc in event.docs) {
-        restList.add(Restaurant.fromFirestore(doc, null));
-      }
-    },
+    await db.collection("restaurants").get().then(
+      (event) {
+        int i = 0;
+        for (var doc in event.docs) {
+          restList.add(Restaurant.fromFirestore(doc, null));
+        }
+      },
       onError: (e) => print("Error getting doc"),
     );
   }
 
-
   Future<FirebaseApp> _initializeFirebase() async {
     FirebaseOptions firebaseConfig = DefaultFirebaseOptions.currentPlatform;
     FirebaseApp firebaseApp =
-    await Firebase.initializeApp(options: firebaseConfig);
+        await Firebase.initializeApp(options: firebaseConfig);
 
     return firebaseApp;
   }
-
-
 
   int _selectedIndex = 1;
   static const TextStyle optionStyle =
@@ -117,7 +111,7 @@ class _CheckInPageState extends State<CheckPage> {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
+//<<<<<<< HEAD
     return MaterialApp(
       home: Scaffold(
         bottomNavigationBar: BottomNavigationBar(
@@ -149,71 +143,96 @@ class _CheckInPageState extends State<CheckPage> {
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: const CameraPosition(
-            target: LatLng(0, 0),
-            zoom: 2,
+            target: LatLng(40.441504010988524, -79.95637135482303),
+            zoom: 15,
           ),
+          // ElevatedButton(
+          //           style: ElevatedButton.styleFrom(
+          //               backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          //               padding:
+          //               const EdgeInsets.symmetric(horizontal: 77, vertical: 30),
+          //               textStyle: const TextStyle(
+          //                   color: Colors.grey,
+          //                   fontSize: 25,
+          //                   fontWeight: FontWeight.bold)),
+          //           onPressed: () {
+          //             print("test");
+          //           },
+          //           child: const Text('Choose Restuarant',
+          //               style: TextStyle(
+          //                 color: Color.fromARGB(255, 68, 98, 56),
+          //               )),
+          //         )]
+
+          // ),
+
           markers: _markers.values.toSet(),
-=======
-    test();
-    return Scaffold(
-      backgroundColor:
-          Color.fromARGB(255, 119, 195, 91), //0x86c66f // gray 0x8d948b
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            //_pages:
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Check In',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.filter_alt),
-            label: 'Filters',
-          ),
-        ],
-        //unselectedItemColor: Colors.amber,
-        currentIndex: _selectedIndex,
-        selectedItemColor: Color.fromARGB(255, 119, 195, 91),
-        onTap: _onItemTapped,
-      ),
-
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 119, 174, 98),
-        automaticallyImplyLeading: false,
-        title: new Image.asset("assets/images/Logo.png"),
-      ),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-        const SizedBox(
-        height: 50, // <-- SEE HERE
->>>>>>> main
         ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Color.fromARGB(255, 255, 255, 255),
-              padding:
-              const EdgeInsets.symmetric(horizontal: 77, vertical: 30),
-              textStyle: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold)),
-          onPressed: () {
-            print("test");
-          },
-          child: const Text('Choose Restuarant',
-              style: TextStyle(
-                color: Color.fromARGB(255, 68, 98, 56),
-              )),
-        )]
-        )
-      ),
+        // ),
+
+        // test();
+        //     return Scaffold(
+        // home: Scaffold(
+        //  backgroundColor:
+        //     Color.fromARGB(255, 119, 195, 91), //0x86c66f // gray 0x8d948b
+        // bottomNavigationBar: BottomNavigationBar(
+        //   items: const <BottomNavigationBarItem>[
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.home),
+        //       label: 'Home',
+        //       //_pages:
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.search),
+        //       label: 'Check In',
+        //     ),
+        //     BottomNavigationBarItem(
+        //       icon: Icon(Icons.filter_alt),
+        //       label: 'Filters',
+        //     ),
+        //   ],
+        //   //unselectedItemColor: Colors.amber,
+        //   currentIndex: _selectedIndex,
+        //   selectedItemColor: Color.fromARGB(255, 119, 195, 91),
+        //   onTap: _onItemTapped,
+        // ),
+
+        // appBar: AppBar(
+        //   backgroundColor: Color.fromARGB(255, 119, 174, 98),
+        //   automaticallyImplyLeading: false,
+        //   title: new Image.asset("assets/images/Logo.png"),
+        // ),
+
+        // body: Center(
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     crossAxisAlignment: CrossAxisAlignment.center,
+        //     children: <Widget>[
+        //   const SizedBox(
+        //   height: 50, // <-- SEE HERE
+        //>>>>>>> main
+        // ),
+        // ElevatedButton(
+        //   style: ElevatedButton.styleFrom(
+        //       backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        //       padding:
+        //       const EdgeInsets.symmetric(horizontal: 77, vertical: 30),
+        //       textStyle: const TextStyle(
+        //           color: Colors.grey,
+        //           fontSize: 25,
+        //           fontWeight: FontWeight.bold)),
+        //   onPressed: () {
+        //     print("test");
+        //   },
+        //   child: const Text('Choose Restuarant',
+        //       style: TextStyle(
+        //         color: Color.fromARGB(255, 68, 98, 56),
+        //       )),
+        // )] ///childreb
+        //   ), //child
+        // ), //body
+      ), //scaffold
     );
+    //  );
   }
 }
